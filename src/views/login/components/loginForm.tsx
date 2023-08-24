1b0ae0af183efd37280529dda8945abb7ffdfafc
linhuizi <529246402@qq.com>
2023-08-24 20:53:36 +0800

    fix: 更新登录页

--- src/views/login/components/loginForm.tsx
+++ src/views/login/components/loginForm.tsx
@@ -1,9 +1,55 @@
-import { Form } from 'antd'
+import { Button, Form, Input, message } from 'antd'
+import { useNavigate } from 'react-router-dom'
+
+interface FieldType {
+  username?: string
+  password?: string
+  remember?: string
+}
 
 function LoginForm() {
+  const navigate = useNavigate()
+
+  function onFinish(values: FieldType) {
+    message.success('登录成功！')
+    // 页面跳转
+    navigate('/home/index')
+  }
+  function onFinishFailed(errorInfo: any) {
+    // console.log('Failed:', errorInfo)
+  }
   return (
-  <Form>
-   <h1>Login Form</h1>
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="用户名"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input placeholder="用户名：admin / user"/>
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="密码"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
   </Form>
   )
 }
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,8 +1,18 @@
 import LoginForm from './components/loginForm'
+import './index.less'
 
 function Login() {
   return (
-    <LoginForm/>
+  <div className="login-container">
+    <div className="login-box">
+      <div className="login-form">
+        <div className="login-logo">
+          <span className="logo-text">React-Admin</span>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  </div>
   )
 }
 
